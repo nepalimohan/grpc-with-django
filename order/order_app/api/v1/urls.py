2e3694ce0c424f5b24cbdea19768508e87dff094
Mohan Nepali <nepalimohan@outlook.com>
2025-06-13 15:27:15 +0545

    order

--- order/order_app/api/v1/urls.py
+++ order/order_app/api/v1/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
+from . import views
+from django.urls import include
+
+router = DefaultRouter()
+router.register('orders', views.OrderViewSet, basename='orders')
 
 urlpatterns = [
-    
+    path('', include(router.urls))
 ]
